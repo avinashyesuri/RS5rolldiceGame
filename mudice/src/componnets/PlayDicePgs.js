a7c1cf65f9bb871ba6dec0cb7e03661cce8ff028
Avinashhola <avinash.hola9@outlook.com>
2024-07-18 16:46:33 +0530

    changes done for duce

--- mudice/src/componnets/PlayDicePgs.js
+++ mudice/src/componnets/PlayDicePgs.js
@@ -1,9 +1,16 @@
 import React from 'react'
-
+import './AllPags.css';
+import TtoaslScore from './TotalScore';
+import ArrayScore from './ArrayScore';
 const PlayDicePgs = (props) => {   //Not   DESTRUCTURED
   return (
     <div>
-      <h1>PlayDicePgs</h1>
+      <div style={{ margin: '10px', display: 'flex', flexDirection: "row", justifyContent: "space-between" }}>
+
+        <TtoaslScore />
+        <ArrayScore />
+
+      </div>
       <button onClick={props.toggle}>Back 'X'!</button>
     </div>
   )
---
+++ mudice/src/componnets/TotalScore.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './AllPags.css';
+const TtoaslScore = () => {
+    return (
+        <div> <div className='playDiceMain'>
+            <h1 className='numericalSty'>0</h1>
+            <button>Total ScoreE--</button>
+        </div></div>
+    )
+}
+
+export default TtoaslScore
